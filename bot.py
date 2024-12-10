cd74fe93258668114a3710d44077c3823872d4cb
kaoekb <kaoekb@gmail.com>
2024-12-10 17:47:58 +0300

    env

--- bot.py
+++ bot.py
@@ -1,18 +1,23 @@
 import logging
 import datetime
+import os
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.daily import DailyTrigger
+from dotenv import load_dotenv  # Импортируем для работы с .env
+
+# Загружаем переменные из .env файла
+load_dotenv()
 
 # Включаем логирование для удобства отладки
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Токен бота
-API_TOKEN = 'YOUR_BOT_TOKEN'
+# Получаем переменные окружения из .env
+API_TOKEN = os.getenv('TELEGRAM_API_TOKEN')
+GROUP_CHAT_ID = os.getenv('GROUP_CHAT_ID')
 
 # ID стикеров для утреннего и вечернего времени
 MORNING_STICKER_ID = 'CAACAgIAAxkBAAEBhNJg1DZH_G7ZB_cfwZDPOz5uxtGG2gAC2gAD6SK7Io_OwVmx5n7-VwQ'
@@ -48,14 +53,14 @@ async def cmd_stop(message: types.Message):
 # Функция отправки утреннего стикера
 async def send_morning_sticker():
     if bot_enabled:
-        # Отправляем стикер в группу, замените chat_id на ID вашей группы
-        await bot.send_sticker(chat_id='YOUR_GROUP_CHAT_ID', sticker=MORNING_STICKER_ID)
+        # Отправляем стикер в группу
+        await bot.send_sticker(chat_id=GROUP_CHAT_ID, sticker=MORNING_STICKER_ID)
 
 # Функция отправки вечернего стикера
 async def send_evening_sticker():
     if bot_enabled:
-        # Отправляем стикер в группу, замените chat_id на ID вашей группы
-        await bot.send_sticker(chat_id='YOUR_GROUP_CHAT_ID', sticker=EVENING_STICKER_ID)
+        # Отправляем стикер в группу
+        await bot.send_sticker(chat_id=GROUP_CHAT_ID, sticker=EVENING_STICKER_ID)
 
 # Планирование задач (утренний и вечерний стикеры)
 def schedule_jobs():
