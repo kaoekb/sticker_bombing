c4a642422370bf973f8113ae7072e7c62ecf199b
kaoekb <kaoekb@gmail.com>
2024-12-13 19:38:22 +0300

    help up

--- bot.py
+++ bot.py
@@ -126,17 +126,7 @@ def cmd_help(message):
         bot.send_message(chat_id, HELP_MESSAGE)
     # Здесь можно добавить логику по обработке других типов сообщений
 
-def cmd_start(message):
-    global bot_enabled
-    bot_enabled = True
-    bot.reply_to(message, START_MESSAGE)
-    schedule_jobs(message.chat.id)  # Передаем chat_id для планировщика
 
-# @bot.message_handler(commands=['quote'])
-# def cmd_quote(message):
-#     quote = random.choice(memes)
-#     bot.reply_to(message, quote)
-    
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     chat_id = message.chat.id  # Получаем chat_id из сообщения
